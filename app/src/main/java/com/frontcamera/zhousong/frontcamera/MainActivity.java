7e951f331db551c52cd652e5bbd999f47fca9678
Yuejun <15625102008@163.com>
2017-10-10 21:15:27 +0800

    Second commit

--- app/src/main/java/com/frontcamera/zhousong/frontcamera/MainActivity.java
+++ app/src/main/java/com/frontcamera/zhousong/frontcamera/MainActivity.java
@@ -5,15 +5,23 @@ import android.content.Context;
 import android.os.Bundle;
 import android.view.SurfaceView;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class MainActivity extends Activity {
     Context context = MainActivity.this;
     SurfaceView surfaceView;
     CameraSurfaceHolder mCameraSurfaceHolder = new CameraSurfaceHolder();
+    private final Timer timer = new Timer();
+    private int count=0;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         initView();
+        timer.schedule(task,0,1000);
     }
 
     public void initView()
@@ -22,5 +30,17 @@ public class MainActivity extends Activity {
         surfaceView = (SurfaceView) findViewById(R.id.surfaceView1);
         mCameraSurfaceHolder.setCameraSurfaceHolder(context,surfaceView);
     }
+
+    TimerTask  task= new TimerTask() {
+        @Override
+        public void run() {
+            if(count==10){
+                finish();
+            }
+            count++;
+        }
+    };
+
+
 
 }
